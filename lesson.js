25c40ceb6b73bf686c6bc43a4dd1e7dd65c2bd8a
Ates06 <ates.qurbanov06@gmail.com>
2023-02-27 01:12:17 +0400

    finish

---
+++ lesson.js
@@ -0,0 +1,8 @@
+function demo(){
+
+    var input = document.getElementById("a").value
+    document.getElementById('d1').innerHTML = input
+ 
+    var input = document.getElementById("b").value
+    document.getElementById('d2').innerHTML = input
+ }
